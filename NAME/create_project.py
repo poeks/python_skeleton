9bfc96e96474e2753ff020f009759c30a6bb3c87
Jen Oslislo <poeksweb@gmail.com>
2016-08-22 12:09:55 -0700

    create project script

--- NAME/create_project.py
+++ NAME/create_project.py
@@ -2,22 +2,27 @@ from subprocess import call
 import os
 
 project = raw_input("Name of project (Assuming desired path is '..'): ")
-if not project.isalnum():
+pwd = os.environ['PWD']
+path = pwd + "/../" + project
+
+reserved = ["bin", "build", "data", "dist", "docs", "NAME", 
+        "projectname.egg-info", "tests", "venv"]
+if not project.isalnum() or os.path.exists(path) or project in str(reserved): #any(project in s for s in reserved):
     print "Not a valid name"
     exit()
 
 zipname = "master.zip"
 reponame = "python_skeleton"
 repo = "https://github.com/poeks/"+ reponame +"/archive/" + zipname
-home = os.environ['PWD']
-path = home + "/../" + project
 
 print "Making new project " + project + " at " + path
-call(["mkdir", path])
-os.chdir(path)
 
 f = open(zipname, "w")
 call(["curl", "-L", repo], stdout=f)
 call(["unzip", zipname])
-call(["mv", reponame + "/*", path])
 
+call(["mv", reponame + "-master", project])
+call(["mv", project, path])
+
+call(["rm", zipname])
+call(["rm", reponame + "-master", "-rf"])
